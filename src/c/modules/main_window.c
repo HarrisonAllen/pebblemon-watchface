411b3f1ad1ba75a6c6038be32f1e2ac7a654c87d
Harrison Allen <hmallen@mit.edu>
2021-07-26 02:26:31 -0400

    clay settings

--- src/c/modules/main_window.c
+++ src/c/modules/main_window.c
@@ -15,6 +15,8 @@ static GRect s_inner_rect;
 static GRect s_inner_rect_white;
 static GPoint s_hour_endpoint, s_min_endpoint;
 
+ClaySettings settings;
+
 /* Game loading handlers */
 static void load_game() {
   Pokemon_initialize(s_graphics);
@@ -26,7 +28,9 @@ static void unload_game() {
 
 /* Input handlers*/
 static void accel_tap_handler(AccelAxisType axis, int32_t direction) {
-  Pokemon_handle_tap(s_graphics);
+  if (settings.ShakeSprite) {
+    Pokemon_handle_tap(s_graphics);
+  }
 }
 
 /* timer callback */
@@ -80,10 +84,10 @@ static void foreground_update_proc(Layer *layer, GContext *ctx) {
 
   // Draw the top and bottom halves of the pokeball frame
   GRect radial_bounds = GRect(bounds.origin.x - 30, bounds.origin.y - 29, bounds.size.w + 61, bounds.size.h + 59);
-  graphics_context_set_fill_color(ctx, PBL_IF_COLOR_ELSE(GColorRed, GColorLightGray));
+  graphics_context_set_fill_color(ctx, settings.BackgroundTopColor);
   graphics_fill_radial(ctx, radial_bounds, GOvalScaleModeFillCircle, PBL_IF_ROUND_ELSE(49, 43), 
                         DEG_TO_TRIGANGLE(-90), DEG_TO_TRIGANGLE(90));
-  graphics_context_set_fill_color(ctx, GColorWhite);
+  graphics_context_set_fill_color(ctx, settings.BackgroundBottomcolor);
   graphics_fill_radial(ctx, radial_bounds, GOvalScaleModeFillCircle, PBL_IF_ROUND_ELSE(49, 43), 
                         DEG_TO_TRIGANGLE(90), DEG_TO_TRIGANGLE(270));
 
@@ -98,45 +102,73 @@ static void foreground_update_proc(Layer *layer, GContext *ctx) {
 #endif
 
   // Draw the outer circles
-  graphics_context_set_stroke_color(ctx, GColorWhite);
-  graphics_context_set_stroke_width(ctx, 2);
-  graphics_draw_circle(ctx, center, 72);
-  
+  if (settings.BatteryBar) {
+    BatteryChargeState battery_state = battery_state_service_peek();
+    GColor battery_color;
+    if (battery_state.is_charging) {
+      battery_color = PBL_IF_COLOR_ELSE(GColorBlueMoon, GColorLightGray);
+    } else if (battery_state.charge_percent <= 10) {
+      battery_color = PBL_IF_COLOR_ELSE(GColorRed, GColorLightGray);
+    } else if (battery_state.charge_percent <= 20) {
+      battery_color = PBL_IF_COLOR_ELSE(GColorChromeYellow, GColorLightGray);
+    } else {
+      battery_color = PBL_IF_COLOR_ELSE(GColorIslamicGreen, GColorLightGray);
+    }
+
+    int length = (battery_state.charge_percent * TRIG_MAX_ANGLE) / 100;
+    GRect battery_bounds = SCREEN_BOUNDS_SQUARE;
+    battery_bounds.origin.x -= 1;
+    battery_bounds.origin.y -= 1;
+    battery_bounds.size.w += 3;
+    battery_bounds.size.h += 3;
+    graphics_context_set_fill_color(ctx, GColorWhite);
+    graphics_fill_radial(ctx, battery_bounds, GOvalScaleModeFitCircle, 3, 
+                          0, TRIG_MAX_ANGLE);
+    graphics_context_set_fill_color(ctx, battery_color);
+    graphics_fill_radial(ctx, battery_bounds, GOvalScaleModeFitCircle, 3, 
+                          TRIG_MAX_ANGLE-length, TRIG_MAX_ANGLE);
+  } else {
+    graphics_context_set_stroke_color(ctx, GColorWhite);
+    graphics_context_set_stroke_width(ctx, 2);
+    graphics_draw_circle(ctx, center, 72);
+  }
   graphics_context_set_stroke_color(ctx, GColorBlack);
   graphics_context_set_stroke_width(ctx, 1);
   graphics_draw_circle(ctx, center, 70);
   graphics_draw_circle(ctx, center, 74);
-  
+
   // Draw circles for the hours
-  GPoint point;
-  GRect rect;
-  for (uint8_t i = 0; i < 12; i++) {
-    graphics_context_set_fill_color(ctx, GColorWhite);
-    point = gpoint_from_polar(s_adjusted_screen_rect, GOvalScaleModeFillCircle, DEG_TO_TRIGANGLE(i*30));
-    graphics_fill_circle(ctx, point, i % 3 == 0 ? 6 : 4);
-    
-    // The cardinal hours get pokeballs!
-    if (i % 3 == 0) {
-      graphics_context_set_fill_color(ctx, PBL_IF_COLOR_ELSE(GColorRed, GColorLightGray));
-      rect = grect_centered_from_polar(s_adjusted_screen_rect, GOvalScaleModeFillCircle, DEG_TO_TRIGANGLE(i*30), GSize(13, 13));
-      rect.origin.y += 1;
-      graphics_fill_radial(ctx, rect, GOvalScaleModeFillCircle, 6, DEG_TO_TRIGANGLE(-90), DEG_TO_TRIGANGLE(90));
+  if (settings.HourTickmarks) {
+    GPoint point;
+    GRect rect;
+    for (uint8_t i = 0; i < 12; i++) {
+      graphics_context_set_fill_color(ctx, GColorWhite);
+      point = gpoint_from_polar(s_adjusted_screen_rect, GOvalScaleModeFillCircle, DEG_TO_TRIGANGLE(i*30));
+      graphics_fill_circle(ctx, point, i % 3 == 0 ? 6 : 4);
+      
+      // The cardinal hours get pokeballs!
+      if (i % 3 == 0) {
+        graphics_context_set_fill_color(ctx, PBL_IF_COLOR_ELSE(GColorRed, GColorLightGray));
+        rect = grect_centered_from_polar(s_adjusted_screen_rect, GOvalScaleModeFillCircle, DEG_TO_TRIGANGLE(i*30), GSize(13, 13));
+        rect.origin.y += 1;
+        graphics_fill_radial(ctx, rect, GOvalScaleModeFillCircle, 6, DEG_TO_TRIGANGLE(-90), DEG_TO_TRIGANGLE(90));
+
+        graphics_context_set_stroke_width(ctx, 1);
+        graphics_context_set_stroke_color(ctx, GColorBlack);
+        graphics_draw_line(ctx, GPoint(point.x-5, point.y), GPoint(point.x+5, point.y));
+        graphics_context_set_stroke_color(ctx, GColorWhite);
+        graphics_draw_line(ctx, GPoint(point.x-5, point.y+1), GPoint(point.x+5, point.y+1));
+        
+        graphics_context_set_fill_color(ctx, GColorWhite);
+        graphics_fill_circle(ctx, point, 2);
+        graphics_context_set_stroke_color(ctx, GColorBlack);
+        graphics_draw_circle(ctx, point, 2);
+      }
 
       graphics_context_set_stroke_width(ctx, 1);
       graphics_context_set_stroke_color(ctx, GColorBlack);
-      graphics_draw_line(ctx, GPoint(point.x-5, point.y), GPoint(point.x+5, point.y));
-      graphics_context_set_stroke_color(ctx, GColorWhite);
-      graphics_draw_line(ctx, GPoint(point.x-5, point.y+1), GPoint(point.x+5, point.y+1));
-      
-      graphics_context_set_fill_color(ctx, GColorWhite);
-      graphics_fill_circle(ctx, point, 2);
-      graphics_context_set_stroke_color(ctx, GColorBlack);
-      graphics_draw_circle(ctx, point, 2);
+      graphics_draw_circle(ctx, point, i % 3 == 0 ? 6 : 4);
     }
-
-    graphics_context_set_stroke_width(ctx, 1);
-    graphics_context_set_stroke_color(ctx, GColorBlack);
-    graphics_draw_circle(ctx, point, i % 3 == 0 ? 6 : 4);
   }
 
   // Draw the white outline for the minute hand
@@ -148,7 +180,7 @@ static void foreground_update_proc(Layer *layer, GContext *ctx) {
   min_startpoint = gpoint_from_polar(s_inner_rect_white, GOvalScaleModeFitCircle, s_minute_angle + DEG_TO_TRIGANGLE(48));
   graphics_draw_line(ctx, min_startpoint, s_min_endpoint);
   // Then draw the actual minute hand
-  graphics_context_set_stroke_color(ctx, GColorBlack);
+  graphics_context_set_stroke_color(ctx, settings.MinuteColor);
   graphics_context_set_stroke_width(ctx, 2);
   for (int8_t i = -45; i < 46; i+=9) {
     min_startpoint = gpoint_from_polar(s_inner_rect, GOvalScaleModeFitCircle, s_minute_angle + DEG_TO_TRIGANGLE(i));
@@ -164,7 +196,7 @@ static void foreground_update_proc(Layer *layer, GContext *ctx) {
   hour_startpoint = gpoint_from_polar(s_inner_rect_white, GOvalScaleModeFitCircle, s_hour_angle + DEG_TO_TRIGANGLE(48));
   graphics_draw_line(ctx, hour_startpoint, s_hour_endpoint);
   // And draw the actual hour hand
-  graphics_context_set_stroke_color(ctx, PBL_IF_COLOR_ELSE(GColorRed, GColorBlack));
+  graphics_context_set_stroke_color(ctx, settings.HourColor);
   graphics_context_set_stroke_width(ctx, 2);
   for (int8_t i = -45; i < 46; i+=9) {
     hour_startpoint = gpoint_from_polar(s_inner_rect, GOvalScaleModeFitCircle, s_hour_angle + DEG_TO_TRIGANGLE(i));
@@ -210,6 +242,79 @@ static void window_unload(Window *window) {
   window_destroy(s_window);
 }
 
+static void default_settings() {
+  settings.BackgroundBottomcolor = GColorWhite;
+  settings.BackgroundTopColor = PBL_IF_COLOR_ELSE(GColorRed, GColorLightGray);
+  settings.HourColor = PBL_IF_COLOR_ELSE(GColorRed, GColorBlack);
+  settings.MinuteColor = GColorBlack;
+  settings.Analog = true;
+  settings.HourTickmarks = true;
+  settings.ShakeSprite = true;
+  settings.BatteryBar = false;
+}
+
+static void load_settings() {
+  default_settings();
+
+  persist_read_data(CLAY_SAVE_KEY, &settings, sizeof(settings));
+}
+
+static void save_settings() {
+  persist_write_data(CLAY_SAVE_KEY, &settings, sizeof(settings));
+
+  layer_mark_dirty(s_foreground_layer);
+}
+
+static void inbox_received_handler(DictionaryIterator *iter, void *context) {
+  // Read color preferences
+  Tuple *bg_top_color_t = dict_find(iter, MESSAGE_KEY_BackgroundTopColor);
+  if(bg_top_color_t) {
+    settings.BackgroundTopColor = GColorFromHEX(bg_top_color_t->value->int32);
+  }
+
+  Tuple *bg_bot_color_t = dict_find(iter, MESSAGE_KEY_BackgroundBottomColor);
+  if(bg_bot_color_t) {
+    settings.BackgroundBottomcolor = GColorFromHEX(bg_bot_color_t->value->int32);
+  }
+
+  Tuple *hour_color_t = dict_find(iter, MESSAGE_KEY_HourColor);
+  if(hour_color_t) {
+    settings.HourColor = GColorFromHEX(hour_color_t->value->int32);
+  }
+
+  Tuple *minute_color_t = dict_find(iter, MESSAGE_KEY_MinuteColor);
+  if(minute_color_t) {
+    settings.MinuteColor = GColorFromHEX(minute_color_t->value->int32);
+  }
+
+  Tuple *time_style_t = dict_find(iter, MESSAGE_KEY_TimeStyle);
+  if(time_style_t) {
+    char * time_style = time_style_t->value->cstring;
+    settings.Analog = strcmp(time_style, "analog") == 0;
+  }
+
+  Tuple *hour_tickmarks_t = dict_find(iter, MESSAGE_KEY_HourTickmarks);
+  if(hour_tickmarks_t) {
+    settings.HourTickmarks = hour_tickmarks_t->value->int32 == 1;
+  }
+
+  Tuple *shake_sprite_change_t = dict_find(iter, MESSAGE_KEY_ShakeSpriteChange);
+  if(shake_sprite_change_t) {
+    settings.ShakeSprite = shake_sprite_change_t->value->int32 == 1;
+  }
+
+  Tuple *battery_bar_t = dict_find(iter, MESSAGE_KEY_BatteryBar);
+  if(battery_bar_t) {
+    settings.BatteryBar = battery_bar_t->value->int32 == 1;
+  }
+
+  save_settings();
+}
+
+static void battery_callback(BatteryChargeState state) {
+  layer_mark_dirty(s_foreground_layer);
+}
+
 static void init() {
   // go ahead and calculate these ahead of time, so we don't waste processor time later
   s_screen_rect = SCREEN_BOUNDS_SQUARE;
@@ -223,6 +328,15 @@ static void init() {
   struct tm *cur_time = localtime(&unadjusted_time);
   s_hour = cur_time->tm_hour % 12;
   s_min = cur_time->tm_min;
+
+  battery_state_service_subscribe(battery_callback);
+
+  // Load any previous Clay settings
+  load_settings();
+
+  // Open AppMessage connection
+  app_message_register_inbox_received(inbox_received_handler);
+  app_message_open(256, 256);
 }
 
 void main_window_push() {
--- src/c/modules/main_window.h
+++ src/c/modules/main_window.h
@@ -6,6 +6,19 @@
 // to strange behavior, so I alwyas go with 2
 #define FRAME_DURATION 2
 
+#define CLAY_SAVE_KEY 2
+
+typedef struct ClaySettings {
+    GColor BackgroundTopColor;
+    GColor BackgroundBottomcolor;
+    GColor HourColor;
+    GColor MinuteColor;
+    bool Analog;
+    bool HourTickmarks;
+    bool ShakeSprite;
+    bool BatteryBar;
+} ClaySettings;
+
 void main_window_mark_background_dirty();
 
 void main_window_push();
